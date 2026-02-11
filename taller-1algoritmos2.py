90f967d9baf2ccf2ee9d4c9f361e9b390df1cf75
SamuraiRTX8000 <emmanuelecheverrybarrera6@gmail.com>
2026-02-11 01:33:47 -0500

    funcion resumen y crear nueva lista

--- taller-1algoritmos2.py
+++ taller-1algoritmos2.py
@@ -1,8 +1,10 @@
 notas = [1.6,3.5,2.5,4.0,5.0]
+notas2 = []
 
 def ver_notas(notas):
+    print("Mostrando notas...")
     for i,nota in enumerate(notas):
-        print("Mostrando notas...")
+        
         print(f"Estudiante: {i} {nota}")
 
 
@@ -17,7 +19,7 @@ def modificar_notas(notas):
             for i, nota in enumerate(notas):
                 print(f"Estudiante: {i} {nota}")
             indice = int(input("ingrese el numero del estudiante que quiere modificar"))
-    #asi se asegura de que no ponga un indice que no existe y len medice cuantos elementos hay(indice y numero de elementos son diferentes)
+    #asi se asegura de que no ponga un indice que no existe y len me dice cuantos elementos hay(indice y numero de elementos son diferentes)
             if 0 <= indice and indice <len(notas):
                 nueva_nota = float(input("Ingresa la nueva nota(emtre 1.0 y 5.0)"))
                 if nueva_nota >=1.0 and nueva_nota<=5.0:#asegura que las notas ingresadas esten dentro del rango establecido para cambiarlas
@@ -42,7 +44,52 @@ def modificar_notas(notas):
             for i, nota in enumerate(notas):
                 print(f"Estudiante: {i} {nota} ")#muestra la lista modificada
 
-                
+
+def resumen_notas(notas):
+    aprobados = 0
+    reprobados = 0
+    if len(notas) == 0:
+        print("No hay notas para analizar.")
+    
+    else:
+
+        for nota in notas:
+            if nota >= 3.0:
+                aprobados = aprobados +1
+            else:
+                reprobados = reprobados +1
+        
+        promedio = sum(notas)/ len(notas)
+        porcentaje_aprob = (aprobados/ len(notas))*100
+        porcentaje_reprob = (reprobados / len(notas))*100
+
+        print(f"""Resumen de notas
+            
+            Aprobados: {aprobados}
+            
+            Reprobados: {reprobados}
+            
+            Promedio de la clase: {promedio}
+
+            Porcentaje de aprobados: {porcentaje_aprob}
+
+            Porcentaje de reprobados: {porcentaje_reprob}  """)
+    
+
+
+def crear_lista():
+        notas2 = []
+
+        vueltas = int(input("ingrese cuatas notas quiere agregar a la lista "))
+        for i in range(vueltas):#
+                print("La nota de estar entre 1.0 y 5.0")
+                nueva_nota = float(input(f"Ingrese la nota: {i})"))
+                if nueva_nota >=1.0 and nueva_nota<=5.0:#asegura que las notas que se guarden sean validas
+                    notas2.append(nueva_nota)
+                else:
+                    print("Nota no valida")
+
+        return notas2
 
 
 
@@ -50,7 +97,11 @@ def modificar_notas(notas):
 
 
 
-menu = int(input("""Calificaciones de grupo 1
+
+play = True
+while play == True:
+
+    menu = int(input("""Calificaciones de grupo 1
                  
         1.) Ver notas.
         
@@ -63,4 +114,17 @@ menu = int(input("""Calificaciones de grupo 1
         5.) Salir.
                  
         """))
+    match menu:
+
+        case 1:
+            ver_notas(notas)
+
+        case 2:
+            modificar_notas(notas)
+
+        case 3:
+            resumen_notas(notas)
+
+        case 4:
+            notas = crear_lista()
 
